d0db27af52c598c89ff8a8624d53306a0068d5f8
nwog-jbonilla <jbonilla@neworg.com>
2025-02-06 13:21:55 -0500

    registered new routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,14 @@ const router = express.Router();
 const authRoutes = require('./auth');
 const userRoutes = require('./users');
 const bookRoutes = require('./books');
+const authorRoutes = require('./authors');
+const topicRoutes = require('./topics');  
 
-// Use routes
+// Use routes (without '/api', since it's handled in app.js)
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/books', bookRoutes);
+router.use('/authors', authorRoutes);
+router.use('/topics', topicRoutes);
 
 module.exports = router;
