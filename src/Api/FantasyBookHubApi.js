c8533eb180211305e1add6a4834342ece38eb544
John Bonilla Suarez <bonilla402@gmail.com>
2025-02-10 14:42:55 -0500

    default port

--- src/Api/FantasyBookHubApi.js
+++ src/Api/FantasyBookHubApi.js
@@ -1,6 +1,6 @@
 ﻿import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001/api";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3000/api";
 
 class FantasyBookHubApi {
     // General request function for all API calls
