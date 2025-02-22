def8c36e93b39cfcea60752012bb86fdc73055c0
nwog-jbonilla <jbonilla@neworg.com>
2025-02-21 19:01:55 -0500

    hide empty home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,18 @@
-﻿import React from "react";
+﻿import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../../UserContext";
 import "./Home.css";
 
 const Home = () => {
+    const { user } = useUser();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            navigate("/books");
+        }
+    }, [user, navigate]);
+
     return (
         <div className="home-container">
             <h1>Welcome to Fantasy Book Hub</h1>
